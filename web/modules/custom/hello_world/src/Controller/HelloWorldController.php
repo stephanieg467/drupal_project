ff182fba10d5b3c26665054abf03e3014b46f728
Stephanie Galata <stephanieg467@hotmail.com>
2018-07-26 12:23:03 -0700

    Completed routes and controllers exercise

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -3,21 +3,46 @@
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
 
 /**
- * Class HelloWorldContrller.
+ * Class HelloWorldController.
  *
  * Provide output for the Hello World module.
  */
 class HelloWorldController extends ControllerBase {
 
   /**
-   * Returns a render-able array for a test page.
+   * Returns a render-able array for printing user information.
    */
-  public function content() {
+  public function content(AccountInterface $user) {
+
+    $response = print_r($user, true);
+    $build = [
+      '#markup' => '<pre>'. $response .'</pre>',
+    ];
+    return $build;
+
+  }
+
+  /**
+   * Returns a render-able array for a test admin page.
+   */
+  public function system_admin() {
 
     $build = [
-      '#markup' => 'Hello, World!',
+      '#markup' => 'Hello, World, and Admin!',
+    ];
+    return $build;
+  }
+
+  /**
+   * Returns a render-able array for a full name page.
+   */
+  public function full_name(string $first,string $last) {
+
+    $build = [
+      '#markup' => 'Hello, ' . $first . ' ' . $last . '.',
     ];
     return $build;
   }
