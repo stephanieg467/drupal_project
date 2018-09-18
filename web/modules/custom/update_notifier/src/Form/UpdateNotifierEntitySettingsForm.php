3fe2407247f33e36bbaec2d09215426aaf57ae34
Stephanie Galata <stephanieg467@hotmail.com>
2018-09-18 11:11:35 -0700

    Settings form now extends ConfigFormBase

--- web/modules/custom/update_notifier/src/Form/UpdateNotifierEntitySettingsForm.php
+++ web/modules/custom/update_notifier/src/Form/UpdateNotifierEntitySettingsForm.php
@@ -1,13 +1,24 @@
 <?php
+
 namespace Drupal\update_notifier\Form;
-use Drupal\Core\Form\FormBase;
+
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Class UpdateNotifierEntitySettingsForm.
  *
  * @ingroup update_notifier
  */
-class UpdateNotifierEntitySettingsForm extends FormBase {
+class UpdateNotifierEntitySettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['update_notifier.settings'];
+  }
+
   /**
    * Returns a unique string identifying the form.
    *
@@ -17,17 +28,7 @@ class UpdateNotifierEntitySettingsForm extends FormBase {
   public function getFormId() {
     return 'updatenotifierentity_settings';
   }
-  /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Empty implementation of the abstract submit class.
-  }
+
   /**
    * Defines the settings form for Update notifier entity entities.
    *
@@ -40,17 +41,67 @@ class UpdateNotifierEntitySettingsForm extends FormBase {
    *   Form definition array.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['updatenotifierentity_settings']['#markup'] = 'Settings form for Update notifier entity entities. Manage field settings here.';
 
-    $form['updatenotifierentity_settings']['message'] = [
-      '#title' => $this->t('Messages'),
-      '#type' => 'checkboxes',
-      '#options' => array ([
-        'message1' => $this->t('Message1'),
-        'message2' => $this->t('Message2'),
-      ]),
+    $config = \Drupal::config('update_notifier.settings');
+
+    $form['updatenotifierentity_settings'] = [
+      '#title' => $this->t('Notification Messages'),
+      '#description' => $this->t('Set the messages that are sent for each type of notification.'),
+      '#type' => 'details',
+      '#open' => TRUE,
     ];
 
-    return $form;
+    $form['updatenotifierentity_settings']['price_change_message'] = [
+      '#title' => $this->t('Price Change Message'),
+      '#type' => 'textarea',
+      '#rows' => 5,
+      '#cols' => 3,
+      '#default_value' => $config->get('price_change'),
+    ];
+
+    $form['updatenotifierentity_settings']['on_sale_message'] = [
+      '#title' => $this->t('On Sale Message'),
+      '#type' => 'textarea',
+      '#rows' => 5,
+      '#cols' => 3,
+      '#default_value' => $config->get('on_sale'),
+    ];
+
+    $form['updatenotifierentity_settings']['promotion_message'] = [
+      '#title' => $this->t('Promotion Message'),
+      '#type' => 'textarea',
+      '#rows' => 5,
+      '#cols' => 3,
+      '#default_value' => $config->get('promotion'),
+    ];
+
+    $form['updatenotifierentity_settings']['in_stock_message'] = [
+      '#title' => $this->t('In Stock Message'),
+      '#type' => 'textarea',
+      '#rows' => 5,
+      '#cols' => 3,
+      '#default_value' => $config->get('in_stock'),
+    ];
+
+    return parent::buildForm($form, $form_state);
   }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('update_notifier.settings')
+      ->set('price_change', $form_state->getValue('price_change_message'))
+      ->set('on_sale', $form_state->getValue('on_sale_message'))
+      ->set('promotion', $form_state->getValue('promotion_message'))
+      ->set('in_stock', $form_state->getValue('in_stock_message'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
 }
